461653fcf5efb121c85a6047385b30f4f515c95f
Victor Abu Akleh <Victor.Abuakleh@corp.minted.com>
2020-03-31 13:14:37 -0700

    Added necessary event listeners

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ const amountEl_two = document.getElementById('amount-two');
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
-
-
+// fetch exchange rates and update DOM
 function calculate(){
 
 }
+
+// Even Listeners
+currencyEl_one.addEventListener('change',calculate)
+amountEl_one.addEventListener('input',calculate)
+currencyEl_two.addEventListener('change',calculate)
+amountEl_two.addEventListener('input',calculate)
 
 
 calculate()
